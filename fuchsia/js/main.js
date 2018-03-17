1dd129fb2d14017393a57d5652b7fc885dba1c2b
mgoulao <msilvagoulao@gmail.com>
2018-03-17 19:54:15 +0000

    Add Fake App History

--- fuchsia/js/main.js
+++ fuchsia/js/main.js
@@ -41,6 +41,8 @@ $(document).ready(function () {
     var menu = $("#menu");
     var statusMenu = $("#menu_2");
     var home = $("#home");
+    var appHistory = $("#app_history");
+    var appContainer = $(".app_container");
     var lockScreen = $("#lockscreen");
     var lockOptionsButton = $("#lock_options_button");
     var lockOptions = $(".lock_options");
@@ -50,6 +52,7 @@ $(document).ready(function () {
     var lockHour = $("#lockscreen_hour");
     var batteryLevel = $(".battery_level");
     var batteryIcon = $(".battery_icon");
+    var statusBar = $(".status_bar");
     var statusTime = $(".status_hour");
     var statusDay = $(".status_day");
     var statusUserImage = $(".fuchsia");
@@ -63,8 +66,18 @@ $(document).ready(function () {
     var openedAppObj;
     //Draggable
     var slideHeight = $('body').height() * 0.5;
-    var min = 0;
-    var max = -slideHeight;
+    var homeMin = 0;
+    var homeMax = -slideHeight;
+    var appHistoryslideHeight = -appHistory.height() * 0.75;
+    if ($("body").width() < 800) {
+        appHistoryslideHeight = -appHistory.height() * 0.85;
+        appHistory.css({ "top": appHistoryslideHeight });
+    } else {
+        appHistory.css({ "top": appHistoryslideHeight });
+    }
+    var appHistoryMin = 0;
+    console.log("Max App", appHistory.height());
+    var appHistoryMax = appHistoryslideHeight;
 
     //Date 
     var today = new Date();
@@ -89,6 +102,7 @@ $(document).ready(function () {
         if (statusMenu.hasClass("active")) {
             statusMenu.removeClass("active");
             showHome();
+            showAppHistory();
         } else if (menu.hasClass("active")) {
             menu.removeClass("active");
         }
@@ -96,7 +110,8 @@ $(document).ready(function () {
 
     searchInput.click(function () {
         event.stopPropagation();
-        home.css({ "top": max + "px" });
+        home.css({ "top": homeMax + "px" });
+        appHistory.css({ "transform": "translateY(" + homeMax + "px)" });
     });
 
     lockOptionsButton.click(function () {
@@ -141,10 +156,11 @@ $(document).ready(function () {
         if (statusMenu.hasClass("active")) {
             statusMenu.removeClass("active");
             showHome();
+            showAppHistory();
         } else {
             statusMenu.addClass("active");
             hideHome();
-
+            hideAppHistory();
         }
     });
 
@@ -161,6 +177,7 @@ $(document).ready(function () {
     homeButton.click(function () {
         openedAppObj.close();
         showHome();
+        showAppHistory();
     });
 
     function hideHome() {
@@ -174,6 +191,16 @@ $(document).ready(function () {
         home.css({ "top": "0px" });
         home.css({ "transform": "translateY(0px)" });
         hideNavBar();
+    }
+
+    function hideAppHistory() {
+        appHistory.css({ "top": appHistoryslideHeight + "px" });
+        appHistory.css({ "transform": "translateY(-50%)" });
+    }
+
+    function showAppHistory() {
+        appHistory.css({ "top": appHistoryslideHeight + "px" });
+        appHistory.css({ "transform": "translateY(0px)" });
     }
 
     function showNavBar() {
@@ -190,27 +217,66 @@ $(document).ready(function () {
         openedAppObj = new App(cardId);
     });
 
+    appContainer.bind("taphold", function (e) {
+        console.log("Click Card: ", e.currentTarget.id);
+        var cardId = e.currentTarget.id;
+        openedAppObj = new App(cardId);
+    });
+
     Number.prototype.roundTo = function (nTo) {
         nTo = nTo || 10;
         return Math.round(this * (1 / nTo)) * nTo;
     }
 
-    console.log("Max", max);
+    console.log("Max", homeMax);
     home.draggable({
         axis: "y",
         scroll: false,
         position: 'unset',
         drag: function (event, ui) {
-            if (ui.position.top > min) ui.position.top = min;
-            if (ui.position.top < max) ui.position.top = max;
+            if (ui.position.top < 0) {
+                appHistory.css({ "transform": "translateY(" + ui.position.top + "px)" });
+            }
+            if (ui.position.top > homeMin) ui.position.top = homeMin;
+            if (ui.position.top < homeMax) ui.position.top = homeMax;
         },
         stop: function (event, ui) {
             var topPositionRounded = (ui.position.top).roundTo(slideHeight);
+            appHistory.css({ "transform": "translateY(" + topPositionRounded + "px)" });
 
             $(this).animate({
                 'top': topPositionRounded
             });
         }
+    });
+
+    appHistory.draggable({
+        axis: "y",
+        scroll: false,
+        position: 'unset',
+        drag: function (event, ui) {
+            if (ui.position.top > appHistoryslideHeight + 50) {
+                home.css({ "opacity": 0 });
+                statusBar.css({ "opacity": 0 });
+            } else {
+                home.css({ "opacity": 1 });
+                statusBar.css({ "opacity": 1 });
+            }
+            if (ui.position.top > appHistoryMin) ui.position.top = appHistoryMin;
+            if (ui.position.top < appHistoryMax) ui.position.top = appHistoryMax;
+        },
+        stop: function (event, ui) {
+            if (ui.position.top < appHistoryslideHeight + 120) {
+                home.css({ "opacity": 1 });
+                statusBar.css({ "opacity": 1 });
+
+                var topPositionRounded = (ui.position.top).roundTo(-appHistoryslideHeight);
+                $(this).animate({
+                    'top': topPositionRounded
+                });
+            }
+        }
+
     });
 
     navigator.getBattery().then(function (battery) {
@@ -282,8 +348,57 @@ $(document).ready(function () {
             "title": "Maps",
             "color": "#689df6",
             "content": "Content"
+        },
+        "app1": {
+            "title": "App1",
+            "color": "#3F51B5",
+            "content": "<h2>Content</h2>"
+        },
+        "app2": {
+            "title": "App2",
+            "color": "#00695C",
+            "content": "<h2>Content</h2>"
+        },
+        "app3": {
+            "title": "App3",
+            "color": "#827717",
+            "content": "<h2>Content</h2>"
+        }, "app4": {
+            "title": "App4",
+            "color": "#E65100",
+            "content": "<h2>Content</h2>"
+        },
+        "app5": {
+            "title": "App5",
+            "color": "#1B5E20",
+            "content": "<h2>Content</h2>"
         }
     }
+
+    /*
+    class OpenApps {
+        constructor() {
+            this.appList = {};
+            this.numberOfApps = 0;
+        }
+
+        addApp(app) {
+            if (app instanceof App) {
+                this.appList[this.numberOfApps++] = {
+                    "app": app,
+                    "title": app.title
+                };
+            }
+        }
+        refreshList() {
+            for (var i = 0; i < this.appList.length; i++) {
+                if (i < 2) {
+                    $(".big_cards").append('<div class="history_card"></div>' +
+                        '<h6>App1</h6>');
+                }
+            }
+        }
+    }*/
 
     class App {
         constructor(id) {
@@ -296,16 +411,20 @@ $(document).ready(function () {
         open() {
             console.log("Open App");
             hideHome();
+            hideAppHistory();
             showNavBar();
             openedAppHeader.text(this.title);
-            openedAppHeader.css({"background": this.color});
+            openedAppHeader.css({ "background": this.color });
             openedAppContent.html(this.content);
             openedApp.addClass("active");
         }
         close() {
             console.log("Close App");
             showHome();
+            showAppHistory();
             hideNavBar();
+            home.css({ "opacity": 1 });
+            statusBar.css({ "opacity": 1 });
             openedApp.removeClass("active");
         }
     }
