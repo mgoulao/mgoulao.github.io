bad19b5c5401337ceaf779b0a979410302f6d6e9
mgoulao <msilvagoulao@gmail.com>
2018-03-18 19:07:13 +0000

    Update star wars js

--- scripts/star-wars.js
+++ scripts/star-wars.js
@@ -1,12 +1,3 @@
-/* Learn how to create this and much more with this email course:
-
-https://cssanimation.rocks/courses/animation-101/
-
-MANY THANKS TO @tadywankenobi for the following JS to handle the text in the byline:
-
-The following JS takes in the byline and splits it into letters, each one wrapped in a span. We need to create the spans as nodes, we can't just add them to the HTML using innerHTML, as to do so would mean the CSS won't affect the span because it doesn't recognise the tag as existing. It's an old problem we run into time and again.
-
-*/
 $("document").ready(function () {
 
 
