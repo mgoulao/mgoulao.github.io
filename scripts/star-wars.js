064689e0fd7009c84e771493a13ece46cb8a4686
Manuel Goulão <msilvagoulao@gmail.com>
2019-04-13 14:25:46 +0100

    Update About page

--- scripts/star-wars.js
+++ scripts/star-wars.js
@@ -1,73 +1,53 @@
-$("document").ready(function () {
+window.onload = () => {
 
+    createByLine();
 
-    var introLogo = $(".starwars-demo"), introText = $("#intro_text"), pageContent = $(".page_content"), intro = true;
+    let introLogo = document.querySelector(".starwars-demo"),
+        introText = document.querySelector("#intro_text"),
+        pageContent = document.querySelector(".page_content"),
+        text = true,
+        logo = true,
+        content = true;
 
-
-    if (intro) {
-        introLogo.show();
+    if (text) {
         setTimeout(function () {
-            introText.show();
-        }, 5000);
-
+            introText.style.display = "block"; //show
+        }, 5000); // 5000
+    }
+    if (logo) {
+        introLogo.style.display = "block"; //show
         setTimeout(function () {
-            introLogo.hide();
-        }, 10000);
-
+            introLogo.style.display = "none";//hide
+        }, 8000); // 8000
+    }
+    if (content) {
         setTimeout(function () {
-            pageContent.show();
-        }, 30000);
+            pageContent.style.display = "block"; //show
+        }, 30000); // 30000
         setTimeout(function () {
-            pageContent.css({"opacity": 1});
-        }, 35000);
-    } else {
-        pageContent.show();
-        setTimeout(function () {
-            pageContent.css({"opacity": 1});
-        }, 1000);
+            pageContent.style.opacity = 1; //opacity 1
+        }, 35000); // 35000
     }
 
-    // Add smooth scrolling to all links
-    $("a").on('click', function (event) {
+}
 
-        // Make sure this.hash has a value before overriding default behavior
-        if (this.hash !== "") {
-            // Prevent default anchor click behavior
-            event.preventDefault();
+const createByLine = () => {
+    var byline = document.getElementById('byline');
+    bylineText = byline.innerHTML;
+    bylineArr = bylineText.split('');
+    byline.innerHTML = '';
 
-            // Store hash
-            var hash = this.hash;
+    var span;
+    var letter;
 
-            // Using jQuery's animate() method to add smooth page scroll
-            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 800, function () {
-
-                // Add hash (#) to URL when done scrolling (default click behavior)
-                window.location.hash = hash;
-            });
-        } // End if
-    });
-
-});
-
-
-var byline = document.getElementById('byline');  	// Find the H2
-bylineText = byline.innerHTML;										// Get the content of the H2
-bylineArr = bylineText.split('');									// Split content into array
-byline.innerHTML = '';														// Empty current content
-
-var span;					// Create variables to create elements
-var letter;
-
-for (i = 0; i < bylineArr.length; i++) {									// Loop for every letter
-    span = document.createElement("span");					// Create a <span> element
-    letter = document.createTextNode(bylineArr[i]);	// Create the letter
-    if (bylineArr[i] == ' ') {												// If the letter is a space...
-        byline.appendChild(letter);					// ...Add the space without a span
-    } else {
-        span.appendChild(letter);						// Add the letter to the span
-        byline.appendChild(span); 					// Add the span to the h2
+    for (i = 0; i < bylineArr.length; i++) {
+        span = document.createElement("span");
+        letter = document.createTextNode(bylineArr[i]);
+        if (bylineArr[i] == ' ') {
+            byline.appendChild(letter);
+        } else {
+            span.appendChild(letter);
+            byline.appendChild(span);
+        }
     }
 }
