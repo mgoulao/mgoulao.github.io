37fa4bf40ca4f5f7100e3ecba4c422c65e660eec
Manuel Goulão <msilvagoulao@gmail.com>
2019-09-02 16:14:10 +0100

    update main page

--- scripts/main.js
+++ scripts/main.js
@@ -2,9 +2,18 @@
 
 var projectsList = [
 	{
+		title: "TypeSpaceArcade",
+		type: "Peer-to-Peer Html 5 Game",
+		link: "https://typespacearcade.web.app",
+		content:
+			'An HTML 5 Arcade game that uses Peer-to-Peer technology for multiplayer',
+		icons: [
+			'<svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="#000000" d="M21,6H3A2,2 0 0,0 1,8V16A2,2 0 0,0 3,18H21A2,2 0 0,0 23,16V8A2,2 0 0,0 21,6M21,16H3V8H21M6,15H8V13H10V11H8V9H6V11H4V13H6M14.5,12A1.5,1.5 0 0,1 16,13.5A1.5,1.5 0 0,1 14.5,15A1.5,1.5 0 0,1 13,13.5A1.5,1.5 0 0,1 14.5,12M18.5,9A1.5,1.5 0 0,1 20,10.5A1.5,1.5 0 0,1 18.5,12A1.5,1.5 0 0,1 17,10.5A1.5,1.5 0 0,1 18.5,9Z" /></svg>']
+	},
+	{
 		title: "Shank",
 		type: "Web App",
-		link: "https://shank-swing.firebaseapp.com/",
+		link: "https://shank-swing.web.app/",
 		content:
 			'Shank is a free video analysis tool that helps players improve their game',
 		icons: [
@@ -38,7 +47,7 @@ var projectsList = [
 		content:
 			"A library with cool card swipe concepts",
 		icons: [
-			'<svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="#000000" d="M15,5H14V4H15M10,5H9V4H10M15.53,2.16L16.84,0.85C17.03,0.66 17.03,0.34 16.84,0.14C16.64,-0.05 16.32,-0.05 16.13,0.14L14.65,1.62C13.85,1.23 12.95,1 12,1C11.04,1 10.14,1.23 9.34,1.63L7.85,0.14C7.66,-0.05 7.34,-0.05 7.15,0.14C6.95,0.34 6.95,0.66 7.15,0.85L8.46,2.16C6.97,3.26 6,5 6,7H18C18,5 17,3.25 15.53,2.16M20.5,8A1.5,1.5 0 0,0 19,9.5V16.5A1.5,1.5 0 0,0 20.5,18A1.5,1.5 0 0,0 22,16.5V9.5A1.5,1.5 0 0,0 20.5,8M3.5,8A1.5,1.5 0 0,0 2,9.5V16.5A1.5,1.5 0 0,0 3.5,18A1.5,1.5 0 0,0 5,16.5V9.5A1.5,1.5 0 0,0 3.5,8M6,18A1,1 0 0,0 7,19H8V22.5A1.5,1.5 0 0,0 9.5,24A1.5,1.5 0 0,0 11,22.5V19H13V22.5A1.5,1.5 0 0,0 14.5,24A1.5,1.5 0 0,0 16,22.5V19H17A1,1 0 0,0 18,18V8H6V18Z" /></svg>'
+			'<svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="#000000" d="M16.36,14C16.44,13.34 16.5,12.68 16.5,12C16.5,11.32 16.44,10.66 16.36,10H19.74C19.9,10.64 20,11.31 20,12C20,12.69 19.9,13.36 19.74,14M14.59,19.56C15.19,18.45 15.65,17.25 15.97,16H18.92C17.96,17.65 16.43,18.93 14.59,19.56M14.34,14H9.66C9.56,13.34 9.5,12.68 9.5,12C9.5,11.32 9.56,10.65 9.66,10H14.34C14.43,10.65 14.5,11.32 14.5,12C14.5,12.68 14.43,13.34 14.34,14M12,19.96C11.17,18.76 10.5,17.43 10.09,16H13.91C13.5,17.43 12.83,18.76 12,19.96M8,8H5.08C6.03,6.34 7.57,5.06 9.4,4.44C8.8,5.55 8.35,6.75 8,8M5.08,16H8C8.35,17.25 8.8,18.45 9.4,19.56C7.57,18.93 6.03,17.65 5.08,16M4.26,14C4.1,13.36 4,12.69 4,12C4,11.31 4.1,10.64 4.26,10H7.64C7.56,10.66 7.5,11.32 7.5,12C7.5,12.68 7.56,13.34 7.64,14M12,4.03C12.83,5.23 13.5,6.57 13.91,8H10.09C10.5,6.57 11.17,5.23 12,4.03M18.92,8H15.97C15.65,6.75 15.19,5.55 14.59,4.44C16.43,5.07 17.96,6.34 18.92,8M12,2C6.47,2 2,6.5 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" />'
 		]
 	},
 	{
@@ -81,7 +90,7 @@ $(document).ready(function () {
 
 	//Welcome message for terminal 
 	console.log("%cWelcome to my personal Portefolio",
-		"border-radius:14px; color: #fff; background-color: #0D47A1; padding: 4px 8px;"+
+		"border-radius:14px; color: #fff; background-color: #0D47A1; padding: 4px 8px;" +
 		"font-size: 16px");
 	console.log("You can fork this site template at: " +
 		"https://github.com/mgoulao/Vibrant-Card-Swipe");
@@ -91,6 +100,7 @@ function handleClicks() {
 	var slideCardsContainer = $(".cards-container"),
 		cardList = $("#slider ul"),
 		profileImage = $(".profile-image"),
+		moreMenuBtn = $(".more-menu-btn"),
 		menu = $("#more_menu"),
 		obfuscator = $(".obfuscator"),
 		card = $(".cards-container .card"),
@@ -123,6 +133,7 @@ function handleClicks() {
 	};
 
 	profileImage.click(() => menuMore.open());
+	moreMenuBtn.click(() => menuMore.open());
 	obfuscator.click(() => {
 		menuMore.close();
 		projectDesktop.close();
@@ -220,6 +231,7 @@ function setupDraggableCards() {
 			$(this).clearQueue();
 			if (ui.position.left > min) ui.position.left = min;
 			if (ui.position.left < max) ui.position.left = max;
+			ui.position.top = 0;
 			//Set start drag time
 			if (!startTime) {
 				startTime = Date.now();
